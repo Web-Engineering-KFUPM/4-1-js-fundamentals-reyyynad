ba554a155f5fbd9c5aa71d527687cf338342a4ed
reyyynad <s202276760@kfupm.edu.sa>
2025-09-15 12:18:57 +0300

    Finish Demo 4

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ console.log("%cJS Lab Connected — Start completing the TODOs!", "font-weight:b
 // Task 2.1 — declare & reassign
 // Create let course = "CIS101"; display it usinng console.log(course), reassign to "CIS102"; display it again.
 // write code here
+let course = "CIS101"
+console.log(course)
+course = "CIS102"
+console.log(course)
 
 
 // Task 2.2 — const safety
@@ -14,6 +18,9 @@ console.log("%cJS Lab Connected — Start completing the TODOs!", "font-weight:b
 // NOTE: For now, keep the reassignment line commented so the file runs without errors.
 // const SCHOOL = "MyCollege";
 // SCHOOL = "OtherCollege"; // <-- Uncomment to see the error, then re-comment after testing
+const SCHOOL = "MyCollege";
+// SCHOOL = "OtherCollege";  
+// I got an Uncaught TypeError: Assignment to constant variable, we cannot re-assign const variables
 
 
 // ==========================
@@ -23,11 +30,23 @@ console.log("%cJS Lab Connected — Start completing the TODOs!", "font-weight:b
 // Task 3.1 — arithmetic basics
 // Given let x = 8, y = 3; log x+y, x-y, x*y, x/y, x%y.
 // write code here
+let x =8;
+let y = 3;
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+console.log(x%y);
+
+
 
 
 // Task 3.2 — number vs string
 // Display results of "2" + 3, 2 + "3", and 2 + 3. Add a short comment: why do the first two concatenate?
 // write code here
+console.log("2" + 3);
+console.log(2 + "3");
+console.log(2 + 3); 
 
 
 // Read chapter 4 in zyBooks: Compound Assignment Operators
@@ -45,6 +64,17 @@ console.log("%cJS Lab Connected — Start completing the TODOs!", "font-weight:b
 //         - "Young" if age is between 13 and 35
 //         - "Aged" if age > 35
 // write code here
+let age = prompt("Enter your age:");
+if (age<13){
+    console.log("Child")
+}
+else if  (age >= 13 && age <= 35){
+    console.log("Young")
+}
+else{
+    console.log("Aged")
+}
+
 
 
 // Task 4.2 — Switch statement
@@ -54,8 +84,27 @@ console.log("%cJS Lab Connected — Start completing the TODOs!", "font-weight:b
 //         - If it is "Sat" or "Sun", log "weekend".
 //         - For any other value, log "unknown".
 // write code here
-
-// Read Chapter 4 in zyBooks: Conditional (ternary) operator
+let day = "Mon"
+switch (day) {
+    case "Mon":
+        console.log("weekday")
+        break;
+    case "Tue":
+        console.log("weekday")
+        break;
+    case "Wed":
+        console.log("weekday")
+        break;
+    case "Thu":
+        console.log("weekday")
+        break;    
+    case "Fri":
+        console.log("weekday")
+        break;
+    default:
+        console.log("weekend")
+  }
+// Read Chapter 4 in zyBooks: Conditional (ternary) operatore
 
 // ===============
 // TODO-5: LOOPS
@@ -64,11 +113,21 @@ console.log("%cJS Lab Connected — Start completing the TODOs!", "font-weight:b
 // Task 5.1 — for loop sum
 // TODO: Sum integers 1..10 with a for loop; display the result of total sum.
 // write code here
+let sum = 0
+for(let i = 1; i <= 10; i++){
+    sum = sum + i
+}
+console.log("sum =", sum)
 
 
 // Task 5.2 — while loop
 // let t = 3; while(t > 0), decrement the value t variable in each iteration and display the result.
 // write code here
+let t = 3;
+while (t>0){
+    t = t-1
+    console.log("t =", t)
+}
 
 
 // Read Chapter 4 in zyBooks: Do-While Loop
@@ -82,11 +141,16 @@ console.log("%cJS Lab Connected — Start completing the TODOs!", "font-weight:b
 // Task 6.1 — pure function + return
 // Make a function add(a,b){ return a+b; } display the result of add(2,5).
 // write code here
+function add(a,b){ return a+b; }
+console.log("2 + 5 = ", add(2,5))
 
 
 // Task 6.2 — Arrow functions
 // Make an arrow function; const cube = n => n*n*n; cube(3)
 // write code here
+const cube = n => n*n*n;
+console.log("cube(3) = ",cube(3))
+
 
 
 // =================================
@@ -96,7 +160,12 @@ console.log("%cJS Lab Connected — Start completing the TODOs!", "font-weight:b
 // Task 7.1 — var vs let scope
 // Declare var a = 1; let b = 2 inside a block { },  then try displaying both outside the block; observe differences.
 // write code here
-
+{
+    var a = 1;
+    let b = 2;
+}
+console.log("testing var", a) // this work because var is function scoped
+// console.log("testing let", b) // here i got an error and it is because let is block scoped
 
 // ==================
 // TODO-8: ARRAYS (CORE)
@@ -105,6 +174,14 @@ console.log("%cJS Lab Connected — Start completing the TODOs!", "font-weight:b
 // Task 8.1 — create & mutate
 // TODO: let nums = [3,1,4]; then push(1), unshift(9), pop(); log final array and length.
 // write code here
+let nums = [3,1,4];
+console.log(nums)
+nums.push(1)
+console.log(nums)
+nums.unshift(9)
+console.log(nums)
+nums.pop()
+console.log(nums)
 
 
 // End of manual — great job! Keep this file open and work task by task.
